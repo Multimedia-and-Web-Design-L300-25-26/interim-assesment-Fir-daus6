c691adfb2d55d68f744bcc0bf37cef2225cd46ba
Fir-daus6 <fmmusah001@st.ug.edu.gh>
2026-05-04 00:32:35 +0000

    Refactor CORS middleware to use allowedOrigins

--- server.js
+++ server.js
@@ -14,9 +14,21 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+    'http://localhost:5173',
+    process.env.FRONTEND_URL
+].filter(Boolean);
+
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true, // Allow cookies to be sent with requests
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
 }));
 
 // MongoDB Connection
